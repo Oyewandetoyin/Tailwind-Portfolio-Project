5987d03907b5d2cbe5e13e364e5e4e083f134c26
oyewandetoyin <oyewandetoyin@gmail.com>
2024-11-05 14:57:18 +0100

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens:{
+        "xs": "480px",
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+      }
+    },
+  },
+  plugins: [],
+}
+
